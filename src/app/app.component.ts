59d9d91c8a5a9931cbe72aa7310f0824e6873859
HSP <hspharwinder@gmail.com>
2019-09-05 12:09:03 +0530

    remve extra code and keep simple code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,30 +7,4 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'test-ngx-quill';  
-  blured = false;
-  focused = false;
-
-  created(event) {
-    // tslint:disable-next-line:no-console
-    console.log('editor-created', event);
-  }
-
-  changedEditor(event) {
-    // tslint:disable-next-line:no-console
-    console.log('editor-change', event);
-  }
-
-  focus($event) {
-    // tslint:disable-next-line:no-console
-    console.log('focus', $event);
-    this.focused = true;
-    this.blured = false;
-  }
-
-  blur($event) {
-    // tslint:disable-next-line:no-console
-    console.log('blur', $event);
-    this.focused = false;
-    this.blured = true;
-  }
 }
